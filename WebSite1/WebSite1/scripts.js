c012b7479fdbc419ffb0226b643c748e71f6337d
Vanu Harfield <vanuharfield@gmail.com>
2018-12-15 10:18:31 +1300

    Updated background color. Added comments to data types page and improved readability

--- WebSite1/WebSite1/scripts.js
+++ WebSite1/WebSite1/scripts.js
@@ -1,15 +1,22 @@
+
+/*Gets a string from the user and checks that it has been input with the correct syntax for a JS string*/
 function getString() {
     var userAnswer = prompt("Make the following variable a string: var thisIsAString = ")
+    /*Checks if the user has started their string with correct quotation marks*/
     if ((userAnswer.substr(0, 1) == '\"' && userAnswer.substr(-1) == '\"') || (userAnswer.substr(0,1) == '\'' && userAnswer.substr(-1)) == '\'') {
         document.getElementById("stringResult").innerHTML = "Congratulations! You are correct!"
-    } else {
+    }
+    /*If incorrect input is given returns a tip on how to correct themselves*/
+    else {
         document.getElementById("stringResult").innerHTML = "That is not a string! Remember to use quotation marks when inputting strings. Click the button to try again."
     }
     document.getElementById("stringResult").innerHTML = "Your answer is " + userCorrect;
 }
 
+/*Gets a number input from the user and confirms that it is a number*/
 function getNumber() {
     var userAnswer = prompt("Make the following variable a number var thisIsANumber = ")
+    /*If input is not a number (NaN) suggests the user tries again*/
     if (isNaN(userAnswer)) {
         document.getElementById("numberResult").innerHTML = "Sorry that is not a number. Click the button to try again!"
     } else {
