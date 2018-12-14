d95c2ce41e59b05734b4136db7f33608fa990c40
George Cronin <georgecronin@windowslive.com>
2018-12-14 21:49:33 +1300

    javascript variables page additions

---
+++ WebSite1/WebSite1/scripts.js
@@ -0,0 +1,7 @@
+//Variables learner module function//
+function variablecorrect(input) {
+    if (input.startsWith('var myfirstvariable =') == true);
+        return "Correct";
+    }   else {
+        return "Incorrect. Try Again";
+}
