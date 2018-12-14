5b7ffeb745b93467b24d5b859a592fde3b99fb85
Vanu Harfield <vanuharfield@gmail.com>
2018-12-14 22:25:04 +1300

    Updated button styling for testing area

--- WebSite1/WebSite1/scripts.js
+++ WebSite1/WebSite1/scripts.js
@@ -1,21 +1,18 @@
 function getString() {
     var userAnswer = prompt("Make the following variable a string: var thisIsAString = ")
-    var userCorrect = ""
     if ((userAnswer.substr(0, 1) == '\"' && userAnswer.substr(-1) == '\"') || (userAnswer.substr(0,1) == '\'' && userAnswer.substr(-1)) == '\'') {
-        userCorrect = "correct!"
+        document.getElementById("stringResult").innerHTML = "Congratulations! You are correct!"
     } else {
-        userCorrect = "incorrect :(";
+        document.getElementById("stringResult").innerHTML = "That is not a string! Remember to use quotation marks when inputting strings. Click the button to try again."
     }
     document.getElementById("stringResult").innerHTML = "Your answer is " + userCorrect;
 }
 
 function getNumber() {
     var userAnswer = prompt("Make the following variable a number var thisIsANumber = ")
-    var userCorrect = ""
     if (isNaN(userAnswer)) {
-        userCorrect = "incorrect :("
+        document.getElementById("numberResult").innerHTML = "Sorry that is not a number. Click the button to try again!"
     } else {
-        userCorrect = "correct!"
+        document.getElementById("numberResult").innerHTML = "Congratulations! You are correct!"
     }
-    document.getElementById("numberResult").innerHTML = "Your answer is " + userCorrect;
 }
